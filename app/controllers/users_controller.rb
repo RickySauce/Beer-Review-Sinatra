2bcdc51bc5a7c9d240ff1cdc49105a2dd8d1266f
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-14 18:26:08 +0000

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params["username"])
-    if @user && @user.password.authenticate == params["password"]
+    if @user && @user.authenticate(params["password"])
       session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
     else
@@ -50,6 +50,11 @@ class UsersController < ApplicationController
   get '/users/:id' do
     @user = User.find(params["id"])
     erb :'/users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 
 end
