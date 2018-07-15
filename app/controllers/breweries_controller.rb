0a933b16923054a98a02488c765079240236e505
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-15 20:08:34 +0000

    updated

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,2 +1,16 @@
 class BreweriesController < ApplicationController
+
+  get '/breweries' do
+      erb :'/breweries/breweries'
+  end
+
+  get '/breweries/:id' do
+    @brewery = Brewery.find(params["id"])
+    if @brewery
+      erb :'/breweries/show'
+    else
+      redirect '/breweries'
+    end
+  end
+
 end
